90cb35bde7f4f2af6d9074f8aea120addfbc93ab
Mike Chu <mike@chu.ms>
2019-03-23 16:00:19 +0100

    Code againt puppeteer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const nouns = require('./nouns.json')
-const Nightmare = require('nightmare')
+const puppeteer = require('puppeteer')
 const UserAgent = require('user-agents')
 
 const minNumberOfWords = 2
@@ -30,41 +30,47 @@ const getRandomViewport = () => {
 }
 
 const lucky = async words => {
-  const browser = new Nightmare({ show: false })
   const randomUserAgent = new UserAgent()
-  const randomViewport = getRandomViewport()
-  browser.useragent(randomUserAgent.toString())
-  browser.viewport(randomViewport.width, randomViewport.height)
-
   const encodedQueryWords = encodeURIComponent(words.join(' '))
   // kp=1 Safe Search On https://duckduckgo.com/params
   const url = `https://duckduckgo.com/?kp=1&q=${encodedQueryWords}`
 
-  console.info(`Navigating to: ${url}`)
-  let pagetUrl = ''
-  let pageTitle = ''
-  try {
-    pageUrl = await browser
-      .goto(url)
-      .click('.result__a')
-      .wait(10000) // arbitrary, I know, but a little related extra traffic is cool too.
-      .url()
-    pageTitle = await browser.title().end()
-  } catch (error) {
-    console.error(error)
-  }
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  page.setUserAgent(randomUserAgent.toString())
+  page.setViewport(getRandomViewport())
 
-  console.log(`Internet Noise made at ${new Date()}`)
-  console.log(`Title: ${pageTitle}`)
-  console.log(`URL: ${pageUrl}`)
-  console.log(`Viewport Size: ${randomViewport.width}x${randomViewport.height}`)
-  console.log(`User Agent: ${randomUserAgent}`)
+  console.info(`Navigating to: ${url}`)
+  await page.goto(url)
+  await page.waitFor(10000)
+
+  // let pagetUrl = ''
+  // let pageTitle = ''
+  // try {
+  //   pageUrl = await browser
+  //     .goto(url)
+  //     .click('.result__a')
+  //     .wait(10000) // arbitrary, I know, but a little related extra traffic is cool too.
+  //     .url()
+  //   pageTitle = await browser.title().end()
+  // } catch (error) {
+  //   console.error(error)
+  // }
+
+  await browser.close()
+
+  // console.log(`Internet Noise made at ${new Date()}`)
+  // console.log(`Title: ${pageTitle}`)
+  // console.log(`URL: ${pageUrl}`)
+  // console.log(`Viewport Size: ${randomViewport.width}x${randomViewport.height}`)
+  // console.log(`User Agent: ${randomUserAgent}`)
 }
 
 const getWords = () => {
   const words = []
   const n = randomInterBetween(minNumberOfWords, maxNumberOfWords)
 
+  words.push('!ducky')
   for (let i = 0; i < n; i++) {
     words.push(getRandomWord())
   }
@@ -77,8 +83,8 @@ const delay = async ms => {
 }
 
 const noisify = async () => {
+  // TODO: Make graceful exit
   while (true) {
-    // TODO: Make graceful exit
     await lucky(getWords())
     await delay(10 * 1000)
   }
